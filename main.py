4c205987d7a2b66b5d76065f2bbd640aea8cd8df
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-01 11:52:19 +0900

    show value of near of middle of a module

--- main.py
+++ main.py
@@ -18,6 +18,14 @@ def find_module_information(module_name:str,
 def find_module(module_name:str,hide_single_underscored:bool=True,
                 hide_double_underscored:bool=True,max_depth:int=10,
                 _current_depth=0):
+    
+    def _filter_underscore(current_all,single,double):
+        if double:
+            current_all = [a for a in current_all if not a.startswith('_')]
+        if single:
+            current_all = [a for a in current_all if not a.startswith('__')]
+        return current_all
+
     if _current_depth>max_depth:
         return None
 
@@ -26,10 +34,8 @@ def find_module(module_name:str,hide_single_underscored:bool=True,
         current_all = getattr(current_module,'__all__')
     except:
         current_all = dir(current_module)
-    if hide_double_underscored:
-        current_all = [a for a in current_all if not a.startswith('_')]
-    if hide_single_underscored:
-        current_all = [a for a in current_all if not a.startswith('__')]
+    
+    current_all=_filter_underscore(current_all,hide_single_underscored,hide_double_underscored)
 
     #print(current_all)
     ret={}
@@ -42,6 +48,8 @@ def find_module(module_name:str,hide_single_underscored:bool=True,
         except:
             pass
     if ret:
+        ret['_c__v___dir___v__c_']=_filter_underscore(dir(current_module),hide_single_underscored,hide_double_underscored)
+
         return ret
     else:
         return current_all
