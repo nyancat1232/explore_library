6b2ea16cd34b2a7b1985f5571aa8c45fac4c0541
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-01 11:52:19 +0900

    properly working

--- main.py
+++ main.py
@@ -3,21 +3,44 @@ import sys
 from importlib import import_module
 import pprint
 
-def find_module(module_name:str,hide_double_underscored:bool=False,max_depth:int=2,_current_depth=0):
+def find_module_information(module_name:str,
+                max_depth:int=2,
+                _current_depth=0):
     if _current_depth>max_depth:
         return None
 
     current_module = import_module(module_name)
-    result={}
-    for lib in dir(current_module):
-        if lib.startswith('__') and hide_double_underscored:
-            continue
-            
-        try:
-            result[lib]=find_module(f'{module_name}.{lib}',hide_double_underscored=hide_double_underscored,_current_depth=_current_depth+1)
-        except:
-            result[lib]=getattr(current_module,lib)
-    return result
+    ret = {d:getattr(current_module,d) for d in dir(current_module)}
 
-r=find_module(sys.argv[1],max_depth=3,hide_double_underscored=False)
+    return ret
+
+def find_module(module_name:str,
+                hide_double_underscored:bool=False,max_depth:int=2,
+                _current_depth=0):
+    if _current_depth>max_depth:
+        return None
+
+    current_module = import_module(module_name)
+    try:
+        current_all = getattr(current_module,'__all__')
+    except:
+        current_all = dir(current_module)
+    #print(current_all)
+    ret={}
+    for module in current_all:
+
+        try:
+            
+            ret[module] = find_module(module_name+'.'+module,hide_double_underscored,max_depth,_current_depth=_current_depth+1)
+
+        except:
+            pass
+    if ret:
+        return ret
+    else:
+        return current_all
+
+
+
+r=find_module(sys.argv[1],max_depth=3)
 pprint.pprint(r)
