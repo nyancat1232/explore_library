c8a75d58bd744362bcc6aeb33304e951d28485c2
Lua Jeon <nyancat2323.1232@gmail.com>
2023-12-01 11:52:19 +0900

    arg

--- main.py
+++ main.py
@@ -18,11 +18,11 @@ def find_module(module_name:str,hide_single_underscored:bool=True,
                 hide_double_underscored:bool=True,max_depth:int=10,
                 _current_depth=0):
     
-    def _filter_underscore(current_all,single,double):
-        if double:
-            current_all = [a for a in current_all if not a.startswith('_')]
-        if single:
+    def _filter_underscore(current_all,hide_single,hide_double):
+        if hide_double:
             current_all = [a for a in current_all if not a.startswith('__')]
+        if hide_single:
+            current_all = [a for a in current_all if not a.startswith('_')]
         return current_all
     if _current_depth>max_depth:
         return None
@@ -38,28 +38,36 @@ def find_module(module_name:str,hide_single_underscored:bool=True,
     except:
         current_all = dir(current_module)
 
-    current_all=_filter_underscore(current_all,hide_single_underscored,hide_double_underscored)
+    current_all=_filter_underscore(current_all,hide_single=hide_single_underscored,hide_double=hide_double_underscored)
 
     ret={}
     for module in current_all:
         try:
-            ret[module] = find_module(module_name+'.'+module,hide_double_underscored,max_depth,_current_depth=_current_depth+1)
+            ret[module] = find_module(module_name+'.'+module,
+                                      hide_single_underscored=hide_single_underscored,
+                                      hide_double_underscored=hide_double_underscored,
+                                      max_depth=max_depth,
+                                      _current_depth=_current_depth+1)
         except:
             pass
     
     #sibling modules
     modules_in_dir=[elem for elem in dir(current_module) if type(getattr(current_module,elem)) == ModuleType]
-    modules_in_dir=_filter_underscore(modules_in_dir,hide_single_underscored,hide_double_underscored)
+    modules_in_dir=_filter_underscore(modules_in_dir,hide_single=hide_single_underscored,hide_double=hide_double_underscored)
     for module in modules_in_dir:
         try:
-            ret[module] = find_module(module_name+'.'+module,hide_double_underscored,max_depth,_current_depth=_current_depth+1)
+            ret[module] = find_module(module_name+'.'+module,
+                                      hide_single_underscored=hide_single_underscored,
+                                      hide_double_underscored=hide_double_underscored,
+                                      max_depth=max_depth,
+                                      _current_depth=_current_depth+1)
         except:
             pass
     #print(modules_in_dir)
 
     
     #sibling values
-    ret['_c__v___dir___v__c_']=_filter_underscore(dir(current_module),hide_single_underscored,hide_double_underscored)
+    ret['_c__v___dir___v__c_']=_filter_underscore(dir(current_module),hide_single=hide_single_underscored,hide_double=hide_double_underscored)
     ret['_c__v___dir___v__c_']=[v for v in ret['_c__v___dir___v__c_'] if v not in ret]
 
     return ret
@@ -67,7 +75,10 @@ def find_module(module_name:str,hide_single_underscored:bool=True,
 
 parser = argparse.ArgumentParser()
 parser.add_argument('module',help='a python module for inspecting',type=str)
+parser.add_argument('-nhs',help='not hide single underscore(_)',action="store_false" )
+parser.add_argument('-nhd',help='not hide double underscore(__)',action="store_false" )
 args = parser.parse_args()
-r=find_module(args.module)
+print(args.nhd)
+r=find_module(args.module,hide_double_underscored=args.nhd,hide_single_underscored=args.nhs)
 with open(f'{args.module}.json','w') as json_file:
     json.dump(r,json_file)
